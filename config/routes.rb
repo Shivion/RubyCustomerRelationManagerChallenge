b2fc619b6ba23d8a8246f09654581a41b4759ce3
alexapperley <alexapperley@gmail.com>
2016-11-04 13:47:46 -0500

    Completed part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'finder#index', as: 'finder'
+
+  get '/alphabetized', to: 'finder#alphabetized'
+
+  get '/missing_email', to: 'finder#missing_email'
+
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
